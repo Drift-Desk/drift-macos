c5a1d881a77617029323e8ba2e35677ceff03f2d
rishabh-live <rishabh12536@gmail.com>
2020-08-13 00:56:49 +0530

    minor updates

--- setup.sh
+++ setup.sh
@@ -11,14 +11,20 @@ echo ""
 echo "Installation has started..."
 echo ""
 
-echo "Extarcting Files..."
+echo "Extracting Files..."
 sudo tar -xf drift-resource-files.tar.xz
+echo "Copying Files..."
 sudo cp -R ./drift-resource-files /etc
 sudo cp -R ./drift /bin
 sudo cp -R ./driftc /bin
+sudo rm -r drift-resource-files/
 
 
 
 echo ""
 echo "Installation has Completed..."
 echo ""
+
+sleep 3
+
+clear
